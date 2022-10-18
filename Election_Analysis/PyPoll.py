688e4855f61058945959ecba44673b040710b8ec
rvroomiii <112191580+rvroomiii@users.noreply.github.com>
2022-10-18 16:46:46 -0400

    updated

--- Election_Analysis/PyPoll.py
+++ Election_Analysis/PyPoll.py
@@ -4,21 +4,20 @@
 # 3. The percentage of votes each candidate won.
 # 4. The total numebr of votes each candidate won.
 # 5. The winnner of the election based on popular vote.
+# Add our dependencies.
 import csv
 import os
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# open the election results and read the fie.
-with open(file_to_load) as election_data:
-#print the file object
-   print(election_data)
-# Create a filename variable to a direct or indirect path to the file.
+# Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Use the open statement to open the file as a text file.
-outfile = open(file_to_save, "w")
-# Write some data to the file.
-outfile.write("Hello World")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
 
-# Close the file
-outfile.close()
+# Read the file object with the reader function.
+   file_reader = csv.reader(election_data)
+
+   # Print the header row.
+   headers = next(file_reader)
+   print(headers)
